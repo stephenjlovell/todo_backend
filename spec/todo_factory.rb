0df4bfd68994d52d9a44ebb7c185b5db3ea8eb14
Stephen Lovell <sjlovell34@gmail.com>
2017-02-10 18:00:09 -0500

    update readme. add todo factory

---
+++ spec/todo_factory.rb
@@ -0,0 +1,10 @@
+require 'faker'
+
+FactoryGirl.define do
+
+  factory :todo do
+    sequence(:title) { Faker::Hacker.say_something_smart }
+    sequence(:completed) { Faker::boolean }
+  end
+
+end
